1b477b5122a208d58e5ba113ed140c701019a04e
Akashy0752016 <akashy075@gmail.com>
2025-03-27 11:46:41 +0530

    Update scripts

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -5,10 +5,10 @@ def get_root_folder(path):
   return os.path.dirname(path).split(os.sep)[0]
 
 def process(*args):
-  print(args)
+#  print(args)
   result = [get_root_folder(arg) for arg in args]
   result = list(set(result) - set(['.github']))
-  print(result)
+#  print(result)
   return result
 
 if __name__ == "__main__":
