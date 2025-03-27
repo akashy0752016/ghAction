6c84246ad5efc95f306b34feee6cb49f5c343d36
akashy0752016 <akashy075@gmail.com>
2025-03-27 11:29:25 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -5,8 +5,8 @@ import fire
 def get_root_folder(path):
   return os.path.dirname(path).split(os.sep)[0]
 
-def process(modified-files):
-  result = [get_root_folder(arg) for i, arg in enumerate(modified-files)]
+def process(modified_files):
+  result = [get_root_folder(arg) for i, arg in enumerate(modified_files)]
   return result
 
 if __name__ == "__main__":
